da66a20a751c52f170c8618998f44a70ba3e6820
Daksh <dakshnahar2001@gmail.com>
2023-11-28 11:46:02 +0530

    Refactor: Added constructor and tester to generic class

--- com/bridgelabz/minmax/MaxFinder.java
+++ com/bridgelabz/minmax/MaxFinder.java
@@ -4,20 +4,42 @@ package com.bridgelabz.minmax;
  desc: it provides maximum value for same type of arguments
  */
 public class MaxFinder<T extends Comparable<T>> {
+    private final T var1;
+    private final T var2;
+    private final T var3;
 
     /*
+        @desc: constructor for MaxFinder class
+        @params: three variables for type T
+        @return: none
+     */
+    public MaxFinder(T var1, T var2, T var3){
+        this.var1=var1;
+        this.var2=var2;
+        this.var3=var3;
+    }
+    /*
         @desc: provides maximum out of three
-        @params: three arguments of same type T
+        @params: none
         @return: T
      */
-    public T findMax(T num1, T num2, T num3){
-        T max = num1;
-        if(num2.compareTo(max)>0){
-            max=num2;
+    public T findMax(){
+        T max = var1;
+        if(var2.compareTo(max)>0){
+            max=var2;
         }
-        if(num3.compareTo(max)>0){
-            max=num3;
+        if(var3.compareTo(max)>0){
+            max=var3;
         }
         return max;
+    }
+
+    /*
+        @desc: it calls findMax function to give max for current object
+        @params:none
+        @return: T
+     */
+    public T testFindMax(){
+        return findMax();
     }
 }
