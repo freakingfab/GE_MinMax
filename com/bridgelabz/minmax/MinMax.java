208cfec721ebe101350bca746177e297a9e504b1
Daksh <dakshnahar2001@gmail.com>
2023-11-28 11:20:13 +0530

    Added: UC3 implementing find max function for string values

--- com/bridgelabz/minmax/MinMax.java
+++ com/bridgelabz/minmax/MinMax.java
@@ -4,11 +4,11 @@ public class MinMax {
     public static void main(String[] args){
         System.out.println("Implementing Min Max using java generics");
         System.out.print("TC1: Largest in first position: ");
-        System.out.println(findMaxNumber(5.2F, 4.2F, 3.2F));
+        System.out.println(findMaxNumber("1001", "100", "10"));
         System.out.print("TC2: Largest in second position: ");
-        System.out.println(findMaxNumber(4.2F,5.2F,3.2F));
+        System.out.println(findMaxNumber("100","1001","10"));
         System.out.print("TC3: Largest in third position: ");
-        System.out.println(findMaxNumber(3.2F,4.1F,5.2F));
+        System.out.println(findMaxNumber("100","10","1001"));
     }
 
     /*
@@ -34,6 +34,22 @@ public class MinMax {
      */
     public static Float findMaxNumber(Float num1, Float num2 , Float num3){
         Float max = num1;
+        if(num2.compareTo(max)>0){
+            max=num2;
+        }
+        if(num3.compareTo(max)>0){
+            max=num3;
+        }
+        return max;
+    }
+
+    /*
+       @desc: finds the maximum value among three:
+       @params: takes three string value
+       @return: string
+    */
+    public static String findMaxNumber(String num1, String num2 , String num3){
+        String max = num1;
         if(num2.compareTo(max)>0){
             max=num2;
         }
